1f949e595f6b455cb0e01c2d4eb307d8d72bf182
Tan Le <57321150+tanvle@users.noreply.github.com>
2022-01-23 22:48:49 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { routes } from './routes/index';
-import { db } from './db/index';
+import { routes } from './routes';
+import { db } from './db';
 
 const app = express();
 app.use(express.json());
@@ -10,18 +10,14 @@ routes.forEach(route => {
 });
 
 const start = async () => {
-  try{
     await db.connect('mongodb://localhost:27017');
     await app.listen(8080);
     console.log('Server is listening on port 8080');
-  }
-  catch(err) { 
-    console.log(err);
-  }
 }
 
 start();
 
+// may not need
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`);
   process.exit(1);
